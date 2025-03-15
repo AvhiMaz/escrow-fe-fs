b447a7e543706ff1fb328f7e6bdfb66cea928699
AvhiMaz <avhimazumder5@outlook.com>
2025-03-15 17:29:48 +0530

    feat: update instruction contract
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- anchor/programs/escrow/src/instructions/make.rs
+++ anchor/programs/escrow/src/instructions/make.rs
@@ -1,10 +1,12 @@
-use anchor_lang::prelude::*;
-use anchor_spl::{associated_token::AssociatedToken, token::{transfer_checked, Token, TransferChecked}, token_interface::{Mint, TokenAccount}};
-
 use crate::states::Escrow;
+use anchor_lang::prelude::*;
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
+};
 
 #[derive(Accounts)]
-#[instruction(seed:u64)]
+#[instruction(seed: u64)]
 pub struct Make<'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
@@ -15,22 +17,20 @@ pub struct Make<'info> {
     #[account(
         mut,
         associated_token::mint = mint_a,
-        associated_token::authority = maker, 
+        associated_token::authority = maker,
     )]
-
     pub mint_ata_a: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
         init,
-        space = 8 + Escrow::INIT_SPACE,
         payer = maker,
+        space = 8 + Escrow::INIT_SPACE,
         seeds = [b"escrow", maker.key().as_ref(), seed.to_le_bytes().as_ref()],
         bump
     )]
-
     pub escrow: Account<'info, Escrow>,
 
-     #[account(
+    #[account(
         init,
         payer = maker,
         associated_token::mint = mint_a,
@@ -39,32 +39,31 @@ pub struct Make<'info> {
     pub vault: InterfaceAccount<'info, TokenAccount>,
 
     pub system_program: Program<'info, System>,
-    pub token_program: Program<'info, Token>,
-    pub associated_token_program: Program<'info, AssociatedToken>
-
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
-
-impl<'info> Make<'info>{
-    pub fn init_escrow(&mut self, seed: u64, amount: u64, bumps: &MakeBumps) -> Result<()>{
-        self.escrow.set_inner(Escrow { 
-            seed, 
-            maker: self.maker.key(), 
-            mint_a: self.mint_a.key(), 
-            mint_b: self.mint_b.key(), 
-            receive_amount: amount, 
-            bump: bumps.escrow});
+impl<'info> Make<'info> {
+    pub fn init_escrow(&mut self, seed: u64, amount: u64, bumps: &MakeBumps) -> Result<()> {
+        self.escrow.set_inner(Escrow {
+            seed,
+            maker: self.maker.key(),
+            mint_a: self.mint_a.key(),
+            mint_b: self.mint_b.key(),
+            receive_amount: amount,
+            bump: bumps.escrow,
+        });
         Ok(())
     }
 
-    pub fn deposit(&mut self, deposit_amount: u64) -> Result<()>{
+    pub fn deposit(&mut self, deposit_amount: u64) -> Result<()> {
         let cpi_program = self.token_program.to_account_info();
 
-        let cpi_accounts =  TransferChecked{
+        let cpi_accounts = TransferChecked {
             from: self.mint_ata_a.to_account_info(),
             mint: self.mint_a.to_account_info(),
             to: self.vault.to_account_info(),
-            authority: self.maker.to_account_info(),            
+            authority: self.maker.to_account_info(),
         };
 
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
